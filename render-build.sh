b49bb8a785ef4628d57c20f803f197e8395b1134
MAI GABBAR <141297334+the-gabbar@users.noreply.github.com>
2025-04-03 19:51:39 +0530

    Create render-build.sh

---
+++ render-build.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+echo "Installing Chrome & ChromeDriver..."
+sudo apt update
+sudo apt install -y chromium-browser
+sudo apt install -y chromium-chromedriver
+echo "Chrome & ChromeDriver Installed."
